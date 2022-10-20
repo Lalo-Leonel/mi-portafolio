52207500aed1f35e67f8ca91d1f4720225d5bd56
leonel <lionel.lalo@hotmail.com>
2022-10-20 16:24:49 -0500

    add style in skills

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,15 +1,15 @@
 export const ProgressBar = ({ logoPath, name, percent }) => {
   return (
-    <div className="text-white my-5">
-      <div className="flex flex-row justify-between items-center mb-3">
+    <div className=" text-slate-700 my-5">
+      <div className="flex flex-row justify-between items-center mb-3 h-10">
         <div className="flex flex-row justify-start items-center gap-2">
-          <img src={logoPath} alt="logo" width="40" />
+          <img src={logoPath} alt="logo" width="40"/>
           <div>{name}</div>
         </div>
         <div>{percent}</div>
       </div>
-      <div class="w-full bg-sky-100 h-1">
-        <div class="bg-sky-500 h-1" style={{ width: percent }}></div>
+      <div class="w-full bg-slate-300 h-1">
+        <div class="bg-slate-700 h-1" style={{ width: percent }}></div>
       </div>
     </div>
   );
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -3,9 +3,9 @@ import { ProgressBar } from "../components/ProgressBar";
 
 export const Skills = () => {
   return (
-    <div id="skills" className="h-screen bg-slate-900 pl-10">
+    <div id="skills" className="h-screen bg-slate-900 px-10">
       <SubMenu description={"Habilidades"} />
-      <div className="flex flex-row gap-5 ">
+      <div className="flex flex-row gap-5 border-solid rounded border-2 px-5 mt-1 bg-white">
         <div className="w-1/2">
         <ProgressBar logoPath={"react-logo.png"} name="REACT JS" percent={"90%"}/>
       <ProgressBar logoPath={"html-logo.png"} name="HTML" percent={"90%"}/>
@@ -17,7 +17,7 @@ export const Skills = () => {
         </div>
         <div className="w-1/2">
       <ProgressBar logoPath={"mysql-logo.png"} name="MySQL" percent={"70%"}/>
-      <ProgressBar logoPath={"mongodb-logo.png"} name="MongoDB" percent={"80%"}/>
+      <ProgressBar logoPath={"mongodb-logo.jpg"} name="MongoDB" percent={"80%"}/>
       <ProgressBar logoPath={"redux-logo.png"} name="REDUX" percent={"80%"}/>
       <ProgressBar logoPath={"python-logo.png"} name="PYTHON" percent={"50%"}/>
       <ProgressBar logoPath={"tailwindcss-logo.png"} name="TAILWIND CSS" percent={"90%"}/>
